e11ef281b8fdf27318b9e359dfc8be0bdae2f9b6
NazarovIlya <eliakohai@gmail.com>
2023-07-25 16:27:22 +0300

    updated

--- 03_JavaScript_intro/HW/HW02/script.js
+++ 03_JavaScript_intro/HW/HW02/script.js
@@ -1,39 +1,39 @@
-// // #1
-// let numFirst = 0;
-// let numSecond = 0;
+// #1
+let numFirst = 0;
+let numSecond = 0;
 
-// numFirst = parseInt(prompt('Введите первое число:', 0));
-// console.log(numFirst);
+numFirst = parseInt(prompt('Введите первое число:', 0));
+console.log(numFirst);
 
-// numSecond = parseInt(prompt('Введите второе число:', 0));
-// console.log(numSecond);
+numSecond = parseInt(prompt('Введите второе число:', 0));
+console.log(numSecond);
 
-// if ((numFirst <= 1) && (numSecond >= 3)) {
-//   console.log(true);
-// } else {
-//   console.log(false);
-// }
+if ((numFirst <= 1) && (numSecond >= 3)) {
+  console.log(true);
+} else {
+  console.log(false);
+}
 
-// // #2
-// let test = true;
+// #2
+let test = true;
 
-// (test === true) ? console.log('+++') : console.log('---');
+(test === true) ? console.log('+++') : console.log('---');
 
-// // #3
-// let day = 0;
+// #3
+let day = 0;
 
-// day = parseInt(prompt('Введите число месяца от 1 до 31:', '0-31'));
-// if (day >= 1 && day <= 31) {
-//   if (day >= 1 && day <= 10) {
-//     console.log('Первая декада')
-//   } else if (day >= 11 && day <= 20) {
-//     console.log('Вторая декада')
-//   } else {
-//     console.log('Третья декада');
-//   }
-// } else {
-//   console.log('Ошибка ввода.')
-// }
+day = parseInt(prompt('Введите число месяца от 1 до 31:', '0-31'));
+if (day >= 1 && day <= 31) {
+  if (day >= 1 && day <= 10) {
+    console.log('Первая декада')
+  } else if (day >= 11 && day <= 20) {
+    console.log('Вторая декада')
+  } else {
+    console.log('Третья декада');
+  }
+} else {
+  console.log('Ошибка ввода.')
+}
 
 // #4
 let number = 0;
