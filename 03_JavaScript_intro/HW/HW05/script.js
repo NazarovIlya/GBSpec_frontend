5ecfc3ccad71ad6d4ebeb6c3a9b36932d308e40a
NazarovIlya <eliakohai@gmail.com>
2023-08-05 15:33:23 +0300

    Home work #5 complited

--- 03_JavaScript_intro/HW/HW05/script.js
+++ 03_JavaScript_intro/HW/HW05/script.js
@@ -1,19 +1,19 @@
-// // Задача №1
-// console.log('Задача № 1\n');
-// outputValues = (obj, value) => Object.keys(obj).filter(el=> obj[el] > value).map(el => obj[el]);
+// Задача №1
+console.log('Задача № 1\n');
+outputValues = (obj, value) => Object.keys(obj).filter(el=> obj[el] > value).map(el => obj[el]);
 
-// const numbers = {
-//   keyin1: 1,
-//   keyin2: 2,
-//   keyin3: 3,
-//   keyin4: 4,
-//   keyin5: 5,
-//   keyin6: 6,
-//   keyin7: 7,
-// }
+const numbers = {
+  keyin1: 1,
+  keyin2: 2,
+  keyin3: 3,
+  keyin4: 4,
+  keyin5: 5,
+  keyin6: 6,
+  keyin7: 7,
+}
 
-// console.log(outputValues(numbers, 3));
-// console.log();
+console.log(outputValues(numbers, 3));
+console.log();
 
 
 // Задача №2
@@ -54,6 +54,57 @@ console.log();
 // Задача №3
 console.log('Задача № 3\n');
 
+const products_1 = [
+  {
+    id: 3,
+    price: 200,
+  },
+  {
+    id: 4,
+    price: 900,
+  },
+  {
+    id: 1,
+    price: 1000,
+  },
+];
+
+
+products_1.forEach((el) => el.price = el.price * 0.85)
+console.log(products_1);
+console.log();
+
 
 // Задача №4
 console.log('Задача № 4\n');
+
+const products_2 = [
+  {
+    id: 3,
+    price: 127,
+    photos: [
+      "1.jpg",
+      "2.jpg",
+    ],
+  },
+  {
+    id: 5,
+    price: 499,
+    photos: [],
+  },
+  {
+    id: 10,
+    price: 26,
+    photos: [
+      "3.jpg",
+    ],
+  },
+  {
+    id: 8,
+    price: 78,
+  },
+];
+
+const res = products_2.filter((el) => el.photos != undefined && el.photos != '');
+console.log(res);
+console.log();
