7029f2d60495235f3ed8b67a160bc7db97720292
NazarovIlya <eliakohai@gmail.com>
2023-08-10 21:50:42 +0300

    HW#3 init

---
+++ 04_ECMA_JavaScript/HW03/script.js
@@ -0,0 +1,19 @@
+
+
+// Задача №1
+console.log('Задача № 1\n');
+
+
+console.log('\n');
+
+// Задача №2
+console.log('Задача № 2\n');
+
+
+console.log('\n');
+
+// Задача №3
+console.log('Задача № 3\n');
+
+
+console.log('\n');
