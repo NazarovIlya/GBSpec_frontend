dcba0ba51971c8e2042a6b0d92cae069fd94275d
NazarovIlya <eliakohai@gmail.com>
2023-08-16 12:49:08 +0300

    task #1 of HW #4 - delete the user card - complited

---
+++ 04_ECMA_JavaScript/HW04/script.js
@@ -0,0 +1,41 @@
+// Задача №1
+
+const url = 'https://jsonplaceholder.typicode.com/users';
+const wrap = document.querySelector('.users');
+
+const data = await getData(url);
+console.log(data);
+
+localStorage.clear();
+
+
+
+async function getData(url) {
+  const response = await fetch(url);
+  const result = response.json();
+  return result;
+}
+
+const show = (element) => {
+  wrap.insertAdjacentHTML("beforeend",
+  `<figure class="user" id="${element.id}">
+  <img src="./img/no_photo.jpg" alt="photo">
+  <h2 class="user__name" id="${element.id}">${element.name}</h2>
+  <button class="button__del">Удалить</button>
+</figure>`);
+  localStorage.setItem(element.id, element);
+}
+
+const deleteButton = (button) => button.addEventListener('click', () => {
+  const id = button.parentElement.id;
+  localStorage.removeItem(id);
+  document.getElementById(id).remove();
+})
+
+data.forEach(element => show(element));
+
+const arrayButtons = document.querySelectorAll('.button__del');
+arrayButtons.forEach(element => deleteButton(element));
+
+
+// Задача №2
