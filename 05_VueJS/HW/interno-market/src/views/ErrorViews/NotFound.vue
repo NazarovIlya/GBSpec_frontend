a83fef4a44854e424d1f63542544488773f88f3e
NazarovIlya <eliakohai@gmail.com>
2023-09-09 00:45:00 +0300

    Not Found page in process

---
+++ 05_VueJS/HW/interno-market/src/views/ErrorViews/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>404 Not Found</h1>
+  </div>
+</template>
+
+<script>
+
+export default ({
+  name: 'NotFound',
+});
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
