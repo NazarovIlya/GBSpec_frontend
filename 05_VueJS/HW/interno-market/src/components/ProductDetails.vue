17813db6fa631ca46a23b3f2f7ec631b613235d2
NazarovIlya <eliakohai@gmail.com>
2023-09-12 17:13:19 +0300

    updated

--- 05_VueJS/HW/interno-market/src/components/ProductDetails.vue
+++ 05_VueJS/HW/interno-market/src/components/ProductDetails.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="product">
-    <h1 class="product__title">{{ product.name }}</h1>
+    <h2 class="product__title">{{ this.product.name }}</h2>
     <p class="product__price">Price: {{ formattedPrice }}</p>
+    <p class="text">Available status: {{ available }}</p>
     <p></p>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 
 export default ({
   name: 'ProductDetails',
@@ -21,7 +22,10 @@ export default ({
   },
   computed: {
     formattedPrice() {
-      return `\u0024 ${this.product.price}`;
+      return `\u0024 ${this.product.price.toString()}`;
+    },
+    available() {
+      return this.product.available ? 'Available' : 'Out of stock';
     },
   },
 });
@@ -29,11 +33,18 @@ export default ({
 </script>
 
 <style lang="scss" scoped>
+
+*{
+  margin: 0;
+  padding: 0;
+}
 .product{
+  margin-top: 35px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 25px;
 
   font-size: 18px;
   font-style: bold;
