5b9f54aa59a39d0111d94d4bab80e61500b38e84
NazarovIlya <eliakohai@gmail.com>
2023-09-12 23:41:58 +0300

    product model created

---
+++ 05_VueJS/HW/interno-market/src/models/Product.ts
@@ -0,0 +1,14 @@
+class Product {
+  name: string;
+
+  price: string;
+
+  available: boolean;
+
+  constructor(name: string, price: string, available: boolean) {
+    this.name = name;
+    this.price = price;
+    this.available = available;
+  }
+}
+export default Product;
